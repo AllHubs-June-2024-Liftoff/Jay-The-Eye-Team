0a9f480034d7ffc9fa761366f246e9646b1735ab
bshawlaunch <bradshawdelivery@outlook.com>
2024-12-09 20:03:35 -0600

    Stephen (#1)
    * initial react update
    
    * added java / springboot / dependencies /react libraries

---
+++ recipe-dash-mvc/src/test/java/org/launchcode/recipe_dash_mvc/RecipeDashMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.recipe_dash_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeDashMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
