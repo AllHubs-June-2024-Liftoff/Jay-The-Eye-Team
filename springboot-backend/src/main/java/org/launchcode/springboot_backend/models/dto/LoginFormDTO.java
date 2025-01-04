461998bc84f4343f0f592f1ad40aa8793f9475fc
matthewpatterson211 <56987848+matthewpatterson211@users.noreply.github.com>
2025-01-04 14:30:11 -0600

    logout complete

--- springboot-backend/src/main/java/org/launchcode/springboot_backend/models/dto/LoginFormDTO.java
+++ springboot-backend/src/main/java/org/launchcode/springboot_backend/models/dto/LoginFormDTO.java
@@ -23,4 +23,12 @@ public class LoginFormDTO {
     public void setUsername (String username) {
         this.username = username;
     }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
