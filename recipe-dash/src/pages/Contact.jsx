80fc8f4368d8ca2f80a0f229833ccab26b05e3a5
adamstanislawsky <adamstanislawsky@gmail.com>
2025-01-07 21:35:40 -0600

    Initial contact form setup

--- recipe-dash/src/pages/Contact.jsx
+++ recipe-dash/src/pages/Contact.jsx
@@ -1,8 +1,28 @@
-const Contact = () => {
+import React from 'react';
 
-    <div>
-        <h1>Contact Chef</h1>
-    </div>
+const Contact = () => {
+    return (
+        <div>
+            <h1>Contact Chef</h1>
+            <form>
+                <div>
+                    <label htmlFor="name">Name:</label>
+                        <input type="text" id="name" name="name" required />
+                </div>
+                <div>
+                    <label htmlFor="email">Email:</label>
+                          <input type="email" id="email" name="email" required />
+                </div>
+                 <div>
+                     <label htmlFor="message">Message:</label>
+                          <textarea id="message" name="message" rows="4" required></textarea>
+                 </div>
+            <button type="submit">Send</button>
+
+            </form>
+
+        </div>
+    );
 }
 
 export default Contact;
